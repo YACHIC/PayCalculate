b2d9aa377093ad4dc761f455e3080dd2b4266a72
Gaea <f40670@gmail.com>
2023-03-24 09:44:53 +0800

    Initial Commit

---
+++ PayCalculate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayCalculate
+//
+//  Created by apple on 2023/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
